1b5a0f6d8d19ebeb73a67b807a18101526452858
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-19 16:30:48 +0100

    Feat: errors handling

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 import express from "express";
+import cors from "cors";
+
+import notFound from "./middlewares/notFound.js";
+import errorHandler from "./middlewares/errorHandler.js";
+
+
+
 
 const app = express();
 const port = process.env.SERVER_PORT;
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+};
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+app.use(cors(corsOptions));
+
+
+app.use(express.static("public"));
+
+app.use(express.json());
+
+
+
+app.use(errorHandler);
+app.use(notFound)
+
+app.listen(port, function (error) {
+
+  if (error) {
+    console.log(error);
+  }
+  else {
+    console.log("Server is connected on port " + port);
+  }
+
 });
