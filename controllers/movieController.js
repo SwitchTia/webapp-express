8f36a0ccc695ba965366b7131de36ef051e7f91d
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-20 23:18:41 +0100

    Feat: Show query

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,5 +1,6 @@
 import connection from "../database/dbConnections.js";
 
+
 function index(req, res, next) {
   const query = "SELECT * FROM movies";
 
@@ -11,4 +12,30 @@ function index(req, res, next) {
   });
 }
 
-export default { index };
+function show(req, res, next) {
+  const id = req.params.id;
+  const showQuery = "SELECT * FROM movies WHERE id = ?"
+
+  connection.query(showQuery, [id], (err, movieResult) => {
+    if (err) return next(err);
+    const movie = movieResult[0];
+
+    const reviewsQuery = "SELECT * FROM reviews WHERE movie_id = ?"
+
+    connection.query(reviewsQuery, [id], (err, reviewsResult) => {
+      if (err) return next(err);
+
+      res.status(200);
+      res.json({
+        ...movie,
+        reviews: reviewsResult
+      });
+
+    });
+
+  });
+
+}
+
+export default { index, show };
+
