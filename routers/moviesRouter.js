4330f543f79cf0d994e763cff781baaa06f209c2
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-21 23:00:35 +0100

    Fix: add router for show

--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -6,6 +6,7 @@ import movieController from "../controllers/movieController.js";
 const router = express.Router();
 
 router.get("/", movieController.index);
+router.get("/:id", movieController.show);
 
 
 export default router;
