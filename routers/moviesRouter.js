4f16db91b25f381815f793c51abaeab8a7f697cd
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-22 22:33:42 +0100

    Fix: router storeReview

--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get("/", movieController.index);
 router.get("/search", movieController.search);
 router.get("/:slug", movieController.show);
-// router.post("/:id/reviews", movieController.storeReview);
+router.post("/:id/reviews", movieController.storeReview);
 
 
 
